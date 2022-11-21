22bcdc8f36d5d39cf6853bbf9d09e739140a7e6a
mauro corengia <mcorengia1@gmail.com>
2022-11-21 18:13:24 -0300

    Add remove image function and improve texts

--- main.py
+++ main.py
@@ -56,55 +56,78 @@ if userPreferences.subFolders == "y":
     for (path, dir_names, files_names) in os.walk(
         os.path.dirname(os.path.realpath(__file__))
     ):
-        print(f"Path = {path}, Name = {dir_names}, File = {files_names}")
-
         for fileName in files_names:
             routesAux.append(path + "\\" + fileName)
 
 else:
     for fileName in os.listdir():
-        print(f"fileName = {fileName}")
-        routesAux.append(os.path.dirname(os.path.realpath(__file__)) + "\\" + fileName)
+        routesAux.append(os.path.dirname(
+            os.path.realpath(__file__)) + "\\" + fileName)
 
 routes = tuple(routesAux)
 
 for i in range(0, len(routes)):
-    img1 = cv2.imread(routes[i])
+    try:
+        img1 = cv2.imread(routes[i])
+    except:
+        print('Image already removed')
 
-    # if a write 'if img1' a bug appears
-    if img1 is not None:
+    else:
+        # if a write 'if img1' a bug appears
+        if img1 is not None:
 
-        for j in range(i, len(routes)):
-            img2 = cv2.imread(routes[j])
+            for j in range(i+1, len(routes)):
+                try:
+                    img2 = cv2.imread(routes[j])
+                except:
+                    print('Image already removed')
 
-            if img2 is not None:
+                else:
+                    if img2 is not None:
 
-                if img1.shape == img2.shape:
-                    # They could be duplicates
-                    diff = cv2.subtract(img1, img2)
-                    (
-                        b,
-                        g,
-                        r,
-                    ) = cv2.split(diff)
+                        if img1.shape == img2.shape:
+                            # They could be duplicates
+                            diff = cv2.subtract(img1, img2)
+                            (
+                                b,
+                                g,
+                                r,
+                            ) = cv2.split(diff)
 
-                    if (
-                        cv2.countNonZero(b) == 0
-                        and cv2.countNonZero(g) == 0
-                        and cv2.countNonZero(r) == 0
-                    ):
-                        print("Repeated images found")
-                        # resize the images and show both
-                        img1 = ResizeWithAspectRatio(img1, 400)
-                        img2 = ResizeWithAspectRatio(img2, 400)
+                            if (
+                                cv2.countNonZero(b) == 0
+                                and cv2.countNonZero(g) == 0
+                                and cv2.countNonZero(r) == 0
+                            ):
+                                print("######################\nRepeated images found at \n" +
+                                      routes[i] + '\nand\n' + routes[j] + '\n######################')
 
-                        # combine both images and show
-                        vis = np.concatenate((img1, img2), axis=1)
-                        cv2.imshow("comparation", vis)
+                                # resize the images and show both
+                                img1 = ResizeWithAspectRatio(img1, 400)
+                                img2 = ResizeWithAspectRatio(img2, 400)
 
-                        remove_img = 0
-                        while remove_img != 1 and remove_img != 2 and remove_img != 9:
-                            remove_img = input(
-                                "1. Remove the first img\n2. Remove the second img\n3. Keep both\n"
-                            )
+                                # combine both images and show
+                                vis = np.concatenate((img1, img2), axis=1)
+                                cv2.imshow("comparation", vis)
+                                cv2.setWindowProperty(
+                                    'comparation', cv2.WND_PROP_TOPMOST, 1)
 
+                                key = 0
+                                while key != 49 and key != 50 and key != 51:
+                                    print(
+                                        "1. Remove the first img\n2. Remove the second img\n3. Keep both\n")
+
+                                    key = cv2.waitKey(0)
+                                    if key == 49:
+                                        # number 1 on windows
+                                        os.remove(routes[i])
+
+                                    elif key == 50:
+                                        # number 2 on windows
+                                        os.remove(routes[j])
+
+                                    elif key == 51:
+                                        # number 3 on windows
+                                        continue
+
+                                cv2.destroyAllWindows()
